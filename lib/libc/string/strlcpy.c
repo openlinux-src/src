5f00384f79e87e8266222a26e8f4d567f8e5737f
Kacper Fiedorowicz <fiedorowicz.kacper@gmail.com>
2025-11-17 19:10:43 +0100

    fix(libc): Handle zero `dstsize` in strlcpy implementation

--- lib/libc/string/strlcpy.c
+++ lib/libc/string/strlcpy.c
@@ -3,8 +3,14 @@
 size_t strlcpy(char *restrict dst, const char *restrict src, size_t dstsize)
 {
 	size_t srclen = strlen(src);
+
+	if (dstsize == 0) {
+		return srclen;
+	}
+
 	size_t copylen = srclen < dstsize - 1 ? srclen : dstsize - 1;
 	memcpy(dst, src, copylen);
 	dst[copylen] = '\0';
+
 	return srclen;
 }
